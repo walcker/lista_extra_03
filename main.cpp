b420fc220bfa6a21aa4c061f0a9a9a830cc2f751
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-08-24 17:59:52 +0000

    Imprimindo

--- main.cpp
+++ main.cpp
@@ -17,6 +17,14 @@ int main() {
     usuarios.insert(pair<int, string>(383474924, "João"));
     usuarios.insert(pair<int, string>(56492578095, "Maria"));
     usuarios.insert(pair<int, string>(87465265, "José"));
+
+    // imprimir map usuários
+    map<int, string>::iterator it;
+    for (it = usuarios.begin(); it != usuarios.end(); ++it) {
+        cout << it->first << endl;
+        cout << it->second << endl;
+    }
+    cout << endl;
   
     return 0;
 }
